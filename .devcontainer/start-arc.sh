5b79f8391b979b0fe61d0b85bdfaab2a01fc476e
Kynan Ware <47394200+BagToad@users.noreply.github.com>
2024-04-20 13:19:41 -0600

    Create start-arc.sh

---
+++ .devcontainer/start-arc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+until docker info;
+
+do sleep 1;
+
+done;
+
+minikube start
+
+helm install arc \
+    --namespace "${CONTROLLER_NAMESPACE}" \
+    --create-namespace \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
+helm install "${RUNNER_INSTALLATION_NAME}" \
+    --namespace "${RUNNER_NAMESPACE}" \
+    --create-namespace \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${FAST_ARC_TOKEN}" \
+    --set maxRunners="2" \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
